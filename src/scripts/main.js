483601e6f9811cb6525087ac283e190bf34b65d4
Vadim <vbulka1986@gmail.com>
2019-05-31 23:17:20 +0300

    JS add comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,20 @@
-// ACORDION SECTION REVIEWS
+// ACORDION SECTION MENU
 
-if ($(".menu-acco").length) {
-  $(".menu-acco__trigger").on("click", e => {
+if ($(".menu-acco").length) { // Есть ли на странице элемент с таким классом
+  $(".menu-acco__trigger").on("click", e => { // навешиваем обработчик события по клику на элемент с классом menu-acco__trigger
     e.preventDefault();
-    const element = e.currentTarget;
-    const item = element.parentNode;
-    const allItems = $(".menu-acco__item");
+    const element = e.currentTarget; // Тот элемент на который повесили обработчик
+    const item = element.parentNode; // его родитель - item, блок выше
+    const allItems = $(".menu-acco__item"); // Все айтемы, все элементы списка
 
-    if (!$(item).hasClass("active")) {
-      $(allItems).removeClass("active");
-      $(item).addClass("active");
+    if (!$(item).hasClass("active")) { // Проверяем, если есть на текущем элемете списка класс active, то тогда выполняем то что внутри
+      $(allItems).removeClass("active"); // У всех айтемов удаляем класс active
+      $(item).addClass("active"); // На тот, который кликнули - добавляем
     }
   });
 
-  $(".menu-acco__close").on("click", e => {
+  $(".menu-acco__close").on("click", e => { // Обработчик события по клику на крестик
     e.preventDefault();
-    $(".menu-acco__item").removeClass("active");
+    $(".menu-acco__item").removeClass("active"); // У всех элементов списка удаляется класс active
   });
 }
